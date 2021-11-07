8e3b92919a8f0007ee77911f74cba9b1ebe2a14f
PiciAkk <marci@dbx.hu>
2021-11-07 21:05:03 +0100

    big update in `bubinews.py` -> read description
    code rearranged, dateFont variable removed, `for` cycle now iterates through the `news` list, not through the `range(len(news))` list

--- bubinews/bubinews.py
+++ bubinews/bubinews.py
@@ -7,22 +7,20 @@ import datetime
 from tkhtmlview import HTMLLabel
 
 root = Tk()
-
 root.title("BubiNews")
-dateFont = ("Helvetica", 10)
+
+Label(root, text="BubiNews", font=("Helvetica", 20)).pack()
 
 helpers = openbubi.BubiHelpers()
 news = json.loads(helpers.getNewsFormatted())
 
-Label(root, text="BubiNews", font=("Helvetica", 20)).pack()
-
-for i in range(len(news)):
-    currentTitle = news[i]["title"]
-    currentDate = news[i]["created_time"]
-    currentURL = news[i]["url_webview"]
+for i in news:
+    currentTitle = i["title"]
+    currentDate = i["created_time"]
+    currentURL = i["url_webview"]
     currentDate = datetime.datetime.fromtimestamp(currentDate).strftime('%Y-%m-%d %H:%M:%S')
     headerLabel = HTMLLabel(root, html=f"<a style='font-size: 12px' href='{currentURL}'><p style='text-align: center'>{currentTitle}</p></a>", width=100, height=1.5)
-    dateLabel = Label(root, text=f"({currentDate})", font=dateFont)
+    dateLabel = Label(root, text=f"({currentDate})", font=("Helvetica", 10))
     headerLabel.pack()
     dateLabel.pack()
 
